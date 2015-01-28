656e79bb778b874691b7389372bf09654c77bda9
Jon Tewksbury <jontewks@gmail.com>
2015-01-28 10:08:15 -0800

    Add to git.

---
+++ README.md
@@ -0,0 +1,19 @@
+# poo.js
+
+A library for when you really need poo.
+
+### Usage
+##### Require the module
+```javascript
+var poo = require('poo');
+```
+
+##### In the browser
+```html
+<script type="text/javascript" src="./poo.js"></script>
+```
+
+##### Basic 5
+```javascript
+poo();
+```
---
+++ poo.js
@@ -0,0 +1,22 @@
+(function() {
+  var poo = function() {
+    console.log('    (   )   ');
+    console.log(' (   ) (    ');
+    console.log('  ) _   )   ');
+    console.log('   ( \\_    ');
+    console.log(' _(_\\ \\)__');
+    console.log('(____\\___))');
+  };
+
+  poo.thesaurus = function() {
+    var words = ['crap', 'poop', 'shite', 'BM', 'defecation', 'discharge', 'dung', 'excrement', 'excretion', 'fecal matter', 'feces', 'feculence', 'deuce', 'manure', 'number two', 'stool', 'waste'];
+    var index = Math.floor(Math.random() * words.length);
+    return words[index];
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = poo;
+  } else if (window) {
+    window.poo = poo;
+  }
+}());
